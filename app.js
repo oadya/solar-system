8f8759559c39362af532cb1d7465958db64c7c1c
orphée adya moapa <orphe.adya@gmail.com>
2024-06-03 07:31:57 +0200

    update mogodb url

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
+//mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', {
     //user: process.env.MONGO_USERNAME,
     //pass: process.env.MONGO_PASSWORD,
     user: 'superuser',
