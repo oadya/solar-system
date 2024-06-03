aeb2b03ed26a2d90f2145b48a40faebe7c437f10
orphée adya moapa <orphe.adya@gmail.com>
2024-06-03 07:37:32 +0200

    add env

--- app.js
+++ app.js
@@ -11,12 +11,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-//mongoose.connect(process.env.MONGO_URI, {
-mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', {
-    //user: process.env.MONGO_USERNAME,
-    //pass: process.env.MONGO_PASSWORD,
-    user: 'superuser',
-    pass: 'SuperPassword',
+mongoose.connect(process.env.MONGO_URI, {
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
